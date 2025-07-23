5095b733706c8351328c08e67fe8b1e3b4d29812
ardririy <hynnkgw27@gmail.com>
2025-07-23 18:08:27 +0900

    inputは常にuseする

--- library-verify-problems/library_checker/src/sample/a_plus_b.rs
+++ library-verify-problems/library_checker/src/sample/a_plus_b.rs
@@ -3,7 +3,7 @@
 use library::utils::input::Input;
 
 fn main() {
-    let mut input = library::utils::input::Input::new();
+    let mut input = Input::new();
     let (a,b) = input.pair::<i64,i64>();
     println!("{}", a + b);
 }
