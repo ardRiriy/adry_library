84788c2dd83b60df022a12fd3518c183572d043b
ardririy <hynnkgw27@gmail.com>
2025-12-10 19:23:18 +0900

    choose

---
+++ library/src/math/choose.rs
@@ -0,0 +1,26 @@
+use crate::misc::modint::Modint;
+
+pub struct ChooseMod<const MOD: u64> {
+    v: Vec<Modint<MOD>>,
+}
+
+impl<const MOD: u64> ChooseMod<MOD> {
+    fn new(n: usize) -> Self {
+        let mut v = vec![Modint::new(1); n+1];
+
+        for i in 0..n {
+            v[i+1] = v[i] * (i+1) as u64;
+        }
+
+        Self { v }
+    }
+
+    pub fn ncr(&self, n: usize, r: usize) -> Modint<MOD> {
+        if n < r {
+            return Modint::new(0);
+        }
+
+        self.v[n] / self.v[n-r] / self.v[r]
+    }
+    
+}
--- library/src/math/mod.rs
+++ library/src/math/mod.rs
@@ -1,2 +1,4 @@
+pub mod choose;
 pub mod euclidean;
 pub mod prime;
+
