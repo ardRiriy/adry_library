5e4f8929e64d96405dd6461e7596c5f28e42c74e
ardririy <hynnkgw27@gmail.com>
2026-03-29 16:56:58 +0900

    桁数を取得できるようにした

--- library/src/utils/versatility_functions.rs
+++ library/src/utils/versatility_functions.rs
@@ -22,25 +22,28 @@ where
 }
 
 /// 整数に対してその桁数を返却する
-/// 負数に対する結果は不定
+/// 負数が与えられた場合はErrを返却する
 /// # examples
 /// ```
+/// use library::utils::versatility_functions::digit_count;
 /// let x: i32 = 0;
-/// assert_eq!(x.digit_count(), 1);
+/// assert_eq!(digit_count(x), Ok(1));
 /// let x: i32 = 100;
-/// assert_eq!(x.digit_count(), 3);
+/// assert_eq!(digit_count(x), Ok(3));
 /// let x: i32 = 12345;
-/// assert_eq!(x.digit_count(), 5);
+/// assert_eq!(digit_count(x), Ok(5));
 /// let x: i32 = -12345;
-/// assert_eq!(x.digit_count(), 5);
+/// assert_eq!(digit_count(x), Err(()));
 /// ```
-pub fn digit_count<T>(x: T) -> u32
+pub fn digit_count<T>(x: T) -> Result<u32, ()>
 where
     T: Integer,
 {
     if x == T::zero() {
-        1
+        Ok(1)
+    } else if x > T::zero() {
+        Ok(x.ilog10() + 1)
     } else {
-        x.ilog10() + 1
+        Err(())
     }
 }
