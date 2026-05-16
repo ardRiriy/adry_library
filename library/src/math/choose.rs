9cf67c27cea0b8dbe939e65344fef2b6eb077612
ardririy <hynnkgw27@gmail.com>
2026-05-16 23:12:07 +0900

    逆元の前計算

--- library/src/math/choose.rs
+++ library/src/math/choose.rs
@@ -1,25 +1,27 @@
 use crate::misc::modint::Modint;
 
 pub struct ChooseMod<const MOD: u64> {
-    v: Vec<Modint<MOD>>,
+    fact: Vec<Modint<MOD>>,
+    inv_fact: Vec<Modint<MOD>>,
 }
 
 impl<const MOD: u64> ChooseMod<MOD> {
     pub fn new(n: usize) -> Self {
-        let mut v = vec![Modint::new(1); n + 1];
-
+        let mut fact = vec![Modint::new(1); n + 1];
         for i in 0..n {
-            v[i + 1] = v[i] * (i + 1) as u64;
+            fact[i + 1] = fact[i] * (i + 1) as u64;
         }
-
-        Self { v }
+        let mut inv_fact = vec![Modint::new(1); n + 1];
+        inv_fact[n] = Modint::new(1) / fact[n]; // ここだけ extended_gcd を1回
+        for i in (0..n).rev() {
+            inv_fact[i] = inv_fact[i + 1] * (i + 1) as u64;
+        }
+        Self { fact, inv_fact }
     }
-
     pub fn ncr(&self, n: usize, r: usize) -> Modint<MOD> {
         if n < r {
             return Modint::new(0);
         }
-
-        self.v[n] / self.v[n - r] / self.v[r]
+        self.fact[n] * self.inv_fact[n - r] * self.inv_fact[r]
     }
 }
